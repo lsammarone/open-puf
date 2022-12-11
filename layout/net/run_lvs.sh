f145b8c00ed2fb7cb5ca5735cb5b6fb67ebfde9e
Luke Sammarone <lsammarone@berkeley.edu>
2022-12-10 17:26:28 -0800

    adding NBR schematic

--- layout/net/run_lvs.sh
+++ layout/net/run_lvs.sh
@@ -1,3 +1,3 @@
 # netgen option to exclude fill and tap from source
-export MAGIC_EXT_USE_GDS=1
-netgen -batch lvs "inputs/design_extracted.spice "$design_name"" "inputs/design.lvs.v "$design_name"" inputs/adk/netgen_setup.tcl outputs/lvs_results.log
+# export MAGIC_EXT_USE_GDS=1
+netgen -batch lvs "lvs.spice NBR128_flat" "source.spice NBR128" /farmshare/home/classes/ee/372/PDKs/open_pdks_1.0.310/sky130/sky130A/libs.tech/netgen/sky130A_setup.tcl ./lvs_results.log
